740924ab66c1d4f15cd166594d835c9b616e2d2a
TrungH012006 <trunghuynh.01.2006@gmail.com>
2025-09-10 16:47:28 -0500

    Fix bug with interest computation

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
-function updateRate() 
-{
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("result").innerText = rateval;
-}
-
-function compute()
-{
+function compute() {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
+    var years = document.getElementById("years").value; 
     var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
     var amount = parseInt(principal) + parseFloat(interest);
     var result = document.getElementById("result");
-
-    var year = new Date().getFullYear() + parseInt(years);
-
+    
     if (principal <= 0) {
-        alert("Please enter a positive number!");
+        alert('Please enter a positive number!');
         document.getElementById("principal").focus();
-    } else {
-        result.innerHTML = "If you deposit $" + principal + ",\<br\> at an interest rate of "+ rate + "%,<br\> You will receive an amount of $" + interest+ "<br\> in the year " + year + "<br></br>"
     }
+    else {
+        result.innerHTML = "If you deposit $" + "<mark>" + principal + "</mark>" + ",\<br\> at an interest rate of " + "<mark>" + rate + "%" + "</mark>" + "\<br\> You will receive an amount of $" + "<mark>" + amount + "</mark>" + ",\<br\> in the year " + "<mark>" + year + "</mark>" + "\<br\>";
+    } 
+}
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
 }
